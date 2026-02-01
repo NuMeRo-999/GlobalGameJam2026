fcbbe569023f8fd3fab8a36f0a4d9fcce00c2dd8
NuMeRo-999 <pedrovp003@gmail.com>
2026-02-01 13:57:09 +0100

    camera pan

--- Assets/Scripts/VisionController.cs
+++ Assets/Scripts/VisionController.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 public class VisionController : MonoBehaviour
 {
     public bool hasMask = false;
-    [SerializeField] private GameObject NormalTilemap;
-    [SerializeField] private GameObject HighTilemap;
 
     void Start()
     {
