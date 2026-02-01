4a8deb11f0218ea7e83b981058ee8d019cf188df
NuMeRo-999 <pedrovp003@gmail.com>
2026-02-01 14:53:20 +0100

    Merge branch '17-paneo-camara' of https://github.com/NuMeRo-999/GlobalGameJam2026 into 17-paneo-camara

--- Assets/Scripts/VisionController.cs
+++ Assets/Scripts/VisionController.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class VisionController : MonoBehaviour
 {
-    
+
     public bool hasMask = false;
 
     void Start()
     {
-        
+        // quita y bloquea el cursor al iniciar el juego
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 }
